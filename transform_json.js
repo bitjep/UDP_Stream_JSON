13fb6c16e10e48158614cfdf6921f6d2257c3150
bitjep <julian.plazas@thebitbang.company>
2023-02-22 17:23:17 -0500

    [FIX] Fixed large objects.

--- transform_json.js
+++ transform_json.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const output_folder = path.join("outputs", "transformed");
 const check_time = true;
+let initial_time;
 
 const check_create_folder = async folder_name => {
     try {
@@ -37,20 +38,24 @@ const transform_data_object = data => {
 };
 
 const update_single_json = async (src_file_path, dst_file_path) => {
+    let start_time;
+    if (check_time)
+        start_time = new Date().getTime();
     const file = await fs.readFile(src_file_path);
     const JSON_file = file.toString().split('\n');
-    const new_json_list = [];
+    let new_json_str = "";
     for (const line of JSON_file) {
         if (!line)
             continue;
-        new_json_list.push(transform_data_object(JSON.parse(line).data));
+        new_json_str += JSON.stringify(transform_data_object(JSON.parse(line).data)) + "\n";
     }
-    await fs.writeFile(dst_file_path, JSON.stringify(new_json_list));
+    await fs.writeFile(dst_file_path, new_json_str);
     console.log(`Successfully created ${dst_file_path} from ${src_file_path}.`);
+    if (check_time)
+        console.log(`Partial time = ${new Date().getTime() - start_time} ms.`);
 };
 
 const update_complete_folder = async (input_path, output_path) => {
-    let initial_time;
     if (check_time)
         initial_time = new Date().getTime();
     await check_create_folder(output_path);
